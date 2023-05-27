8d5faf22e28869341db20aae1bf9f71abb6afa30
Camryn Scott <camryn1242@yahoo.com>
2023-05-26 20:52:13 -0400

    implimented /welcome/home page

--- greet/app.py
+++ greet/app.py
@@ -2,6 +2,16 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+@app.route('/')
+def home_page():
+    return """
+    <h1>Welcome to my beginner Website</h1>
+    """
+
 @app.route('/welcome')
 def welcome_page():
     return 'Welcome'
+
+@app.route('/welcome/home')
+def welcome_home():
+    return 'Welcome Home'
