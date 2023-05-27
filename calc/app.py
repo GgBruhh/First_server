96a447771e4ba1d5116df4730f5e800ae4056c2f
Camryn Scott <camryn1242@yahoo.com>
2023-05-26 21:13:46 -0400

    implimented all in one Math functionality

--- calc/app.py
+++ calc/app.py
@@ -36,3 +36,21 @@ def do_div():
     res = div(a, b)
     
     return str(res)
+
+
+operators = {
+        "add": add,
+        "sub": sub,
+        "mult": mult,
+        "div": div,
+        }
+
+@app.route("/math/<oper>")
+def do_math(oper):
+    """Do math on a and b."""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = operators[oper](a, b)
+
+    return str(result)
