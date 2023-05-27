3878e0b0008757716cef1efaa4b9d075b1c183b1
Camryn Scott <camryn1242@yahoo.com>
2023-05-26 21:12:50 -0400

    implimented the /add /sub /mult /div webpages

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,38 @@
 # Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+
+@app.route('/add')
+def do_add():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    res = add(a, b)
+    
+    return str(res)
+
+@app.route('/sub')
+def do_sub():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    res = sub(a, b)
+    
+    return str(res)
+
+@app.route('/mult')
+def do_mult():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    res = mult(a, b)
+    
+    return str(res)
+
+@app.route('/div')
+def do_div():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    res = div(a, b)
+    
+    return str(res)
